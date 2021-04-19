6d4de834d075cab8fa5ebd29b0a4cada7c518431
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2021-04-19 03:29:57 +0200

    coucou
    remis tout en html pcq ça servait à rien de les avoir en .php

---
+++ arborescence/scripts/domaine.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    // $(document).on("scroll", function(e){
+    //     console.log(e);
+    //     if((($(window).scrollTop()) < (($("#scrolled").offset().top)-1)) && flag){
+
+    //         $("html, body").animate({scrollTop: 0}, 750); 
+    //         setTimeout(function(){
+    //             $("body").css({"overflow-y": "hidden"});
+    //         }, 750)
+    //         flag=false;
+    //     }
+
+    // })
+
+    // $(window).scroll(function(){
+    //     $("section").each(function(){
+    //       if ($(window).scrollTop() > $(this).offset().top ) {
+    //         $('.active').removeClass('active');
+    //         $(this).addClass('active');
+    //       }
+    //     });
+    // });
+
+})
