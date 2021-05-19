46e6e5878a0e819fda4a9e1e8bbee54693fc79e7
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-05-19 22:02:18 +0200

    up jeu audiovisuel

--- OLD/mini-jeu/audiovisuel/scripts.js
+++ OLD/mini-jeu/audiovisuel/scripts.js
@@ -1,3 +1,19 @@
 $(document).on("ready",function(){
 
+    $(".pButton").on("click", function(){
+        if($(".pButton>path").attr("d")=="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"){
+            $(".pButton>path").attr("d","M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z");
+            $(".videoPrev").trigger('play');
+        }else{
+            $(".pButton>path").attr("d","M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z");
+            $(".videoPrev").trigger('pause');
+        }
+    });
+
+    $(".videoPrev").on("ended",function(){
+        alert("The audio has ended");
+        $(".videoPrev").trigger('pause');
+        $(".pButton>path").attr("d","M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z");
+    })
+
 });
