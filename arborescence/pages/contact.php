93d283bcca24d3c49214e71b865462c52a71b26f
Rhceline <Rhceline@users.noreply.github.com>
2021-05-20 23:38:01 +0200

    Page contact header

--- arborescence/pages/contact.php
+++ arborescence/pages/contact.php
@@ -6,43 +6,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Contact</title>
+    <link rel="stylesheet" href="../styles/header.css">
     <link rel="stylesheet" href="../styles/contact.css">
 </head>
  
 <body>
-    <header></header>
+
+<header><?php
+        $titre = "";
+        include 'header.php';
+    ?></header>
+
  
     <h1>Contact</h1>
     <div class="container_2">
         <form action="envoi.php" method="POST">
-            <div>
- 
+            
+            <div class="champ">
                 <label for="name">nom </label>
                 <input type="text" id="name" name="name" required />
             </div>
  
-            <div>
+            <div class="champ">
                 <label for="name">prénom </label>
                 <input type="text" id="name" name="prenom" />
             </div>
  
-            <div>
+            <div class="champ">
                 <label for="mail">e-mail </label>
                 <input type="email" id="mail" name="mail" required />
             </div>
  
-            <div>
+            <div class="champ">
                 <label for="cp">code postal </label>
                 <input id="cp" type="text" name ="codepostal" maxlength="5" size="5" placeholder="______" pattern="[0-9]{5}">
             </div>
  
  
-            <div>
+            <div class="champ">
                 <label for="objet">objet </label>
                 <input type="text" id="objet" name="objet" required />
             </div>
  
-            <div>
+            <div class="champ">
                 <label for="msg">message </label>
                 <textarea id="msg" name="message" placeholder="Une question ?" required ></textarea>
             </div>
