a1274f46f72d71e0d1bb2cfc502ff58983012136
Matthislebg <matthis.rousselle@gmail.com>
2021-05-20 19:16:26 +0200

    Domaine
    voila domaine ajout du carroussel

--- arborescence/pages/domaine.php
+++ arborescence/pages/domaine.php
@@ -45,23 +45,10 @@
     <link rel="stylesheet" href="../styles/footer.css">
     <link rel="stylesheet" href="../styles/header.css">
     <link rel="stylesheet" href="../styles/domaine.css">
-    <link rel="stylesheet" href="../styles/jquery.fsscroll.css">
     <link rel="stylesheet" href="../styles/projetmmi.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
-    $('.container').fsScroll({
-    loop:true,
-    keyboard:true,
-    duration: 1000,
-    timing:'linear',
-    index: 1,
-    selectors: {
-    sections:'.sections',
-    section:'.section',
-    page:'.page',
-    active:'.active' }
-})
 </script>    
-<script src="../scripts/jquery.fsscroll.js"></script>
+
     <script src=".../scripts/domaine.js"></script>    
 </head>
 <body>
@@ -170,7 +157,81 @@
 
     <section>
         <h1>PROJET</h1>
-        
+        <div class="carrousel">
+    <h2> <span class="domaineprojet">Audiovisuel</span></h2>
+    <input type="radio" name="diapos" id="radio-1" checked>
+    <input type="radio" name="diapos" id="radio-2">
+    <input type="radio" name="diapos" id="radio-3">
+    <input type="radio" name="diapos" id="radio-4">
+    <ul class="diapos">
+      <!-- DIAPO 1 -->
+      <li class="diapo">
+        <p>
+          <h3>CV Vidéo</h3>
+          <span class="presentation">
+            <img src="https://festival2020.iutmmi.fr/img/project/avc_164.jpg">
+            <p class="description">
+              Ce CV vidéo est un projet commun en audiovisuel et en anglais. Ce n'est pas un CV traditionnel comme on a
+              l'habitude de voir, en effet Anastasiya Balan, ancienne élève à l'IUT Gustave Eiffel a cherché à démontrer
+              ses compétences et son savoir-être sans simplement les énoncer.
+              Ce CV Vidéo présenté au Festival MMI 2020 a obtenu le premier prix dans la catégorie Audiovisuel.
+              <p>
+                <a href="https://www.youtube.com/watch?v=PV3bbVQLLYg&feature=youtu.be&ab_channel=FestivalMMI"> Lien vers
+                  l'oeuvre</a>
+
+          </span>
+        </p>
+      </li>
+      <!-- DIAPO 2 -->
+      <li class="diapo">
+        <p>
+          <h3>Shoot</h3>
+          <span class="presentation">
+            <img src="https://festival2020.iutmmi.fr/img/project/ksm_405.png">
+            Ce projet est un court-métrage réalisé par des anciens élèves en DUT MMI à Limoges en 2020. Cette vidéo
+            raconte l'histoire de Ben, un photographe qui, après avoir essuyé plusieurs échecs, se voit proposer un
+            ultime contrat qui déterminera son avenir dans l’agence. Il n’a qu’un seul cliché à prendre mais cette
+            mission s’annonce périlleuse car la concurrence est rude. Ce court-métrage présenté au Festival MMI 2020 a
+            obtenu un prix dans la catégorie Audiovisuel (2e position) et un prix du public (3e position).
+          </span>
+        </p>
+      </li>
+      <!-- DIAPO 3 -->
+      <li class="diapo">
+        <p>
+          <h3>Super MMI</h3>
+          <span class="presentation">
+            <img src="/medias/audio3.jpg">
+            Super MMI est un projet personnel d'Enzo Cailleton ancien élève en DUT MMI à l'IUT de Laval réalisé de
+            décembre 2019 à avril 2020 (scénario, animation, mixage audio, montage). C'est un court-métrage d'animation
+            3D où l'on retrouve Sylvain un étudiant MMI. Le but de ce projet était de créer une œuvre parlant de la
+            filière MMI de façon originale par rapport à ce qu'on a l'habitude de voir. Ce projet présenté au Festival
+            MMI 2020 a obtenu un prix dans la catégorie Animation (1ère position) et dans la catégorie Son (1ère
+            position)
+          </span>
+        </p>
+      </li>
+      <!-- SLIDE 4 -->
+      <li class="diapo">
+        <p>
+          <h3>Plongée Astrale</h3>
+          <span class="presentation">
+            <img src="https://festival2020.iutmmi.fr/img/project/eps_313.png">
+            Cette animation est un projet personnel d'une ancienne étudiante en DUT MMI à l'IUT de Tarbes. Doralice
+            Macaire a souhaité réaliser un film animé pour le concours Un'Anime 2019 dans le département MMI de Rouen
+            dont le thème imposé était « La tête dans les étoiles ». Ce projet présenté au Festival MMI 2020 a obtenu un
+            prix dans la catégorie Animation (2e position).
+          </span>
+        </p>
+      </li>
+    </ul>
+    <div class="diaposNavigation">
+      <label for="radio-1" id="dotForRadio-1"></label>
+      <label for="radio-2" id="dotForRadio-2"></label>
+      <label for="radio-3" id="dotForRadio-3"></label>
+      <label for="radio-4" id="dotForRadio-4"></label>
+    </div>
+  </div>
     </section>
     <footer><?php 
         include 'footer.php';
