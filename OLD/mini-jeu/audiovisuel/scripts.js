49c8c7a7aced63cc2779cb866aa933ee53e3dec3
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-05-21 22:49:06 +0200

    jeu audiovisuel v1

--- OLD/mini-jeu/audiovisuel/scripts.js
+++ OLD/mini-jeu/audiovisuel/scripts.js
@@ -1,21 +1,84 @@
 $(document).on("ready",function(){
 
+    var actuel;
+    var liens = ["medias/interviews/intro_at.mp4", "medias/interviews/metiers_at.mp4", "medias/interviews/etapes_film_at.mp4", "medias/interviews/pref_at.mp4", "medias/interviews/outro_at.mp4"];
+    var id = ["#intro","#partie1","#partie2","#partie3","#outro"];
+    var newOrdre = [];
+    var actualVideo = 0;
+    var old = [];
+    var diffVid;
+    var vidEtat;
+    var menu = false;
+    refreshList();
+
     $(".pButton").on("click", function(){
         if($(".pButton>path").attr("d")=="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"){
             $(".pButton>path").attr("d","M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z");
             $(".videoPrev").trigger('play');
+            vidEtat = "play";
         }else{
             $(".pButton>path").attr("d","M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z");
             $(".videoPrev").trigger('pause');
+            vidEtat = "pause";
         }
     });
 
-    $(".videoPrev").on("ended",function(){
-        $(".videoPrev").trigger('pause');
-        $(".pButton>path").attr("d","M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z");
+    $(".pReload").on("click", function(){
+        actualVideo = -1;
+        playVid();
+        $(this).css({
+            "transform":"rotate(180deg)"
+        })
+        setTimeout(function(){
+            $(".pReload").css({
+                "transform":"rotate(0deg)"
+            });
+        },500);
+    })
+
+    $(".pSkip").on("click", function(){
+        playVid();
+    })
+    $(".volume").on("input", function(){
+        $(".videoPrev").prop("volume",($(".volume").val())/100);
+    })
+
+    $(".menu").on("click", function(){
+        if(menu){
+            $(".param").css({
+                "width":"0%"
+            });
+            menu = false;
+        }else{
+            $(".param").css({
+                "width":"25%"
+            });
+            menu = true;
+        }
     });
 
-    $(function  () {
+    $(".luminosite").on("input", function(){
+        $(".filtre").css({
+            "background-color": "rgb("+$(".luminosite").val()+","+$(".luminosite").val()+","+$(".luminosite").val()+")"
+        });
+        if(($(".luminosite").val()/127)>1){
+            $("video").css({
+                "opacity": (1-(($(".luminosite").val()/127)-1))
+            })
+        }else{
+            $("video").css({
+                "opacity": ($(".luminosite").val()/127)
+            })
+        }
+    });
+
+    $(".bordure, .bColor").on("input",function(){
+        $("video").css({
+            "border": "solid "+$(".bColor").val()+" "+$(".bordure").val()+"px"
+        });
+    });
+
+    $(function(){
         $( ".items" ).sortable({
             distance:10,
             axis: 'x'
@@ -23,28 +86,14 @@ $(document).on("ready",function(){
         $( ".items" ).disableSelection();
     });
 
-    var actuel;
-    refreshList();
-
-    $(".drag").on("mouseup", function(){    // Le index commence à 0
+    $(document).on("mouseup", function(){    // Pour éviter les bugs, laisser la détection d'action sur le document complet, sans le refresh peut être ignoré
         setTimeout(function(){
             refreshList();
         },10);
     });
 
-    $(".deroulement>li").on("click", function(){
-        $(this).index();
-        if($(this).parent().parent().hasClass("intro")){
-            
-        }else if($(this).parent().parent().hasClass("partie1")){
-            
-        }else if($(this).parent().parent().hasClass("partie2")){
-
-        }else if($(this).parent().parent().hasClass("partie3")){
-
-        }else if($(this).parent().parent().hasClass("outro")){
-
-        }
+    $(document).on("click", ".deroulement>li", function(){
+        rePos($(this).index(), $(this).parent().parent().attr("id"));
     });
 
     function refreshList(){
@@ -56,6 +105,76 @@ $(document).on("ready",function(){
             actuel.children(".deroulement").children("li:nth-child("+(actuel.index()+1)+")").css({
                 "background-color": "#dda200"
             });
+        }
+        buildVideo();
+    }
+
+    function rePos(pos, element){
+
+        if($("#"+element).index()==pos){
+
+        }else{
+
+            $(".items>li:nth-child("+($("#"+element).index()+1)+")").wrap("<div id='delete'></div>");
+            var li = $("#delete").html();
+            $("#delete").remove();
+
+            if(pos==4){
+
+                $(".items>li:nth-child("+(pos)+")").wrap("<div id='delete'></div>");
+                $("#delete").append(li);
+
+            }else{
+                
+                $(".items>li:nth-child("+(pos+1)+")").wrap("<div id='delete'></div>");
+                $("#delete").prepend(li);
+            }
+
+            $("#delete").children().unwrap();
+
+        }
+        refreshList();
+    }
+
+    function buildVideo(){
+        for(var i = 0; i<5; i++){
+            old[i] = newOrdre[i];
+            newOrdre[$(id[i]).index()]= liens[i];
+        }
+
+        for(var i=0; i<5; i++){
+            if(old[i] != newOrdre[i]){
+                diffVid=true;
+            }
+        }
+
+        if(diffVid){
+            diffVid=false;
+            actualVideo = -1;
+            playVid();
+        }
+    }
+
+    $(".videoPrev").on("ended",function(){
+        playVid();
+    });
+
+    function playVid(){
+        actualVideo++
+        if(actualVideo > 4){
+            $(".videoPrev").trigger('pause');
+            $(".pButton>path").attr("d","M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z");
+            actualVideo=0;
+            $(".videoPrev>source").attr("src",newOrdre[actualVideo]);
+            $(".videoPrev")[0].load();
+        }else{
+            $(".videoPrev>source").attr("src",newOrdre[actualVideo]);
+            $(".videoPrev")[0].load();
+            if(vidEtat=="play"){
+                $(".videoPrev").trigger("play");
+            }else{
+                $(".videoPrev").trigger('pause');
+            }
         }
     }
 
