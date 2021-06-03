62660b8c1f1c4480c1588d4e0e290fbf000dcde5
Matthislebg <matthis.rousselle@gmail.com>
2021-06-03 13:57:08 +0200

    modif interview
    modification de l'interview

--- arborescence/pages/interviews.php
+++ arborescence/pages/interviews.php
@@ -10,6 +10,28 @@
     <link rel="stylesheet" href="../styles/interviews.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../scripts/recherche.js"></script>
+<SCRIPT TYPE="text/javascript">$(document).ready(function(){
+ $('.interview_1').on('click',function(){
+   $('.popup_1').css('display','block');
+   $('.background').css('display','flex');
+ })
+$('.interview_2').on('click',function(){
+   $('.popup_2').css('display','block');
+   $('.background').css('display','flex');
+ })
+ $('.interview_3').on('click',function(){
+   $('.popup_3').css('display','block');
+   $('.background').css('display','flex');
+ })
+ $('.interview_4').on('click',function(){
+   $('.popup_4').css('display','block');
+   $('.background').css('display','flex');
+ })
+ $('.popup_button').on('click',function(){
+  $('.popup').css('display','none');
+   $('.background').css('display','none');
+})
+})</SCRIPT>
 </head>
 <body>
     <header>
@@ -18,13 +40,28 @@
         include 'header.php';
         ?>
     </header>
-    <section>            
-        <div>            
-            <p>DESIGN</p>
+    <section>   
+
+        <div class="interview_1"><p>DESIGN</p></div>
+        <div class="interview_2"><p>PROGRAMMATION</p></div>  
+        <div class="interview_3"><p>COMMUNICATION</p></div>       
+        <div class="interview_4"><p>AUDIOVISUEL</p></div>
+        <div class="popup_1 popup">
+            <h2>DESIGN - ROBIN LLOPIS </h2>
+            <iframe width="100%" height="100%" src="<?php  echo $video;  ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
+        <div class="popup_2 popup">
+            <h2>PROGRAMMATION - GAËLLE CHARPENTIER </h2>
+            <iframe width="100%" height="100%" src="<?php  echo $video;  ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
         </div>
-        <div><p>PROGRAMMATION</p></div>  
-        <div><p>COMMUNICATION</p></div>       
-        <div><p>AUDIOVISUEL</p></div>
+        <div class="popup_3 popup">
+        <h2> COMMUNICATION - CLARISSE HENRY  </h2>
+        <iframe width="100%" height="100%" src="<?php  echo $video;  ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
+        </div>
+        <div class="popup_4 popup">
+        <h2> AUDIOVISUEL - ANNE TASSO  </h2>  
+        <iframe width="100%" height="100%" src="<?php  echo $video;  ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </div>
+        </div>
+        <div class="background"></div>
     </section>
 </body>
 </html>
